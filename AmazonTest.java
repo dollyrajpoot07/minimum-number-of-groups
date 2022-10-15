cdf75817e22c0cce77feed817588bcab230a9d4f
Dolly Singh <68997138+dollyrajpoot07@users.noreply.github.com>
2022-10-15 23:18:27 +0530

    Update AmazonTest.java

--- AmazonTest.java
+++ AmazonTest.java
@@ -1,6 +1,9 @@
-// Amazon Prime video is a subscription video on-demand over-the-top streaming and rental service. The team at Prime Video is developing a method to divide movies into groups based on the number theyy have won. A group can consist of any number of movies, but the difference in the number of awards won by any two movies in the group must not exceed k.
-// The movies can be grouped together irrespective of their initial order. Determine the minimum number of groups that can be formed such that each movie is in exactly one group.
-// Example: The numbers of awards per movie are awards = [1, 5, 4, 6, 8, 9, 2], and the maximum allowed difference is k = 3. One way to divide the movies into the minimum number of groups is:
+// Amazon Prime video is a subscription video on-demand over-the-top streaming and rental service. 
+//The team at Prime Video is developing a method to divide movies into groups based on the number theyy have won. 
+//A group can consist of any number of movies, but the difference in the number of awards won by any two movies in the group must not exceed k.
+// The movies can be grouped together irrespective of their initial order. Determine the minimum number of groups that can be formed 
+//such that each movie is in exactly one group. Example: The numbers of awards per movie are awards = [1, 5, 4, 6, 8, 9, 2], 
+// and the maximum allowed difference is k = 3. One way to divide the movies into the minimum number of groups is:
 // 1. The 1st group can contain [2, 1]. the maximum difference between awards of any two movies is 1 which does not exceed k.
 // 2. The 2nd group can contain [5, 4, 6]. The maximum difference between awards of any two movies is 2 which does not exceed k.
 // 3. The 3rd group can contain [8, 9]. The maximum difference between awards of any two movies is 1 which does not exceed k.
